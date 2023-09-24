ea3f567079288983bafdbd993d095207f817a1ef
Richard Walsh <richwalsh2@googlemail.com>
2023-09-24 12:45:51 +0100

    replace athlete register index with code for nodejs proxy

--- web/mooch/athlete/register/index.php
+++ web/mooch/athlete/register/index.php
@@ -3,23 +3,16 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
-
 require dirname(__DIR__, 2) . '/includes/classes/database/DatabaseAthlete.php';
 require dirname(__DIR__, 2) . '/includes/classes/curl/CurlAthleteStats.php';
 require dirname(__DIR__, 2) . '/includes/classes/curl/CurlAthleteNewAuthToken.php';
 require dirname(__DIR__, 2) . '/includes/classes/curl/CurlAthlete.php';
 
-
-
 $databaseAthlete = new DatabaseAthlete();
 $curlAthleteNewAuthToken = new CurlAthleteNewAuthToken();
 
-
 // 1, receive  user UID, client secret, client id, access token, refresh token and email from front end. = $signUpData
-
-$body = file_get_contents('php://input');
-
-$signUpData = json_decode($body, true);
+$signUpData = $_GET;
 
 if($signUpData === null) {
     echo 'signupdata null';
@@ -37,14 +30,14 @@ $tokenExpiresIn = $getTokenExpires['expires_in'];
 // 3, request athlete info from strava to get athleteID, first and last name, profile img, = $athleteProfileData
 $curlAthlete = new CurlAthlete('athlete', ["Authorization: Bearer {$signUpData['accessToken']}"]);
 $athleteProfileData = $curlAthlete->getAthlete();
-
+var_dump($athleteProfileData);
 // 4 send curl request to get ATHLETE STATS FROM STRAVA. = $athleteStatsData
 
 $curlAthleteStats = new CurlAthleteStats('athletes/' . $athleteProfileData['id'] . '/stats', array(
     'Content-Type: application/json', 'Authorization: Bearer ' . $signUpData['accessToken']));
 
 $athleteStatsData = $curlAthleteStats->getAthleteStats();
-
+var_dump($athleteStatsData);
 
 $databaseAthlete->registerAthlete($signUpData['uid'] ,$signUpData['email'] ,$athleteProfileData['id'], $athleteProfileData['firstname'], $athleteProfileData['lastname'], 
 $athleteProfileData['profile_medium'], $tokenExpiresAt, $tokenExpiresIn, $signUpData['clientId'], $signUpData['clientSecret'], $signUpData['accessToken'], $signUpData['refreshToken']);
